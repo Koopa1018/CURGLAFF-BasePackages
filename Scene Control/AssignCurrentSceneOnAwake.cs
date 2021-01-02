912768a7eaba89d75bda42ebbb8825ee315c80b0
Koopa1018 <caleblovestoytrains@gmail.com>
2021-01-01 23:46:26 -0700

    Add AssignCurrentSceneOnAwake, for reset buttons.

---
+++ Scene Control/AssignCurrentSceneOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Clouds.SceneManagement;
+
+/// <summary>
+/// On Awake(), assigns the current active scene to the value of a detected SceneControlAccess.
+/// Useful for making reset buttons, and probably not much else.
+/// </summary>
+public class AssignCurrentSceneOnAwake : MonoBehaviour {
+	//[SerializeField] SceneControlAccess target; //Nope--more convenient for users to add this and let it find SceneControlAccess on its own.
+
+    // Start is called before the first frame update
+    void Awake() {
+        GetComponent<SceneControlAccess>().targetScene = SceneManager.GetActiveScene().name;
+		Destroy(this);
+    }
+}
