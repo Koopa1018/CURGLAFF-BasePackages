a4ec704fb92906db8b01915c6f436f317f39cca8
Koopa1018 <caleblovestoytrains@gmail.com>
2021-01-02 14:22:30 -0700

    Added simple getter for single input button.

---
+++ Input Containers/PlayerButtonInput.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+#if UNITY_NEW_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+using Clouds.PlayerInput;
+
+namespace Clouds.Platformer.CharacterControl {
+	public class PlayerButtonInput : MonoBehaviour, IGenerateInputSignals {
+		#if UNITY_NEW_INPUT_SYSTEM
+		[Header("Inputs")]
+		[SerializeField] InputActionAsset inputMap;
+		[SerializeField] string actionMap = "Weapons";
+		[SerializeField] InputActionReference inputAction;
+		#else
+		[Header("Inputs")]
+		[SerializeField] string buttonName = "Fire1";
+		#endif
+
+		[Header("Output Fields")]
+		[SerializeField] ButtonInputField buttonOutput;
+
+		#if UNITY_NEW_INPUT_SYSTEM
+		void Awake () {
+			inputMap.FindActionMap(actionMap, true).Enable();
+		}
+		#endif
+
+		/// <summary>
+		/// Fetch input from component data.
+		/// NOTE: Sourcing it from here allows for easy switching out of control code:
+		/// should you want AI controlled characters, just write to these from an
+		/// enemy-AI component instead of a player-input-fetching component.
+		/// </summary>
+		public void GenerateInputSignal () {
+			#if UNITY_NEW_INPUT_SYSTEM
+				buttonOutput.Value = inputAction.action.ReadValue<float>() > 0;
+			#else
+				buttonOutput.Value = Input.GetButton (buttonName);
+			#endif
+		}
+
+		public void ClearInputSignal() {
+			buttonOutput.Value = false;
+		} 
+
+	}
+}
