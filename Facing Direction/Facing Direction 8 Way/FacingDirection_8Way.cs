9099b6a1542d80ea9d92a2b0d0d99bb9b10e416d
Koopa1018 <caleblovestoytrains@gmail.com>
2021-01-04 02:09:43 -0700

    Temporarily disable Burst

--- Facing Direction/Facing Direction 8 Way/FacingDirection_8Way.cs
+++ Facing Direction/Facing Direction 8 Way/FacingDirection_8Way.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using Unity.Mathematics;
-using Unity.Burst;
+//using Unity.Burst;
 
 #if UNITY_ENTITIES
 using Unity.Entities;
@@ -30,7 +30,7 @@ namespace Clouds.Facing2D
 		/// Calculates the angle represented by the current facing direction, up to 360* (normalized to 1).
 		/// </summary>
 		/// <returns>The angle represented by the current facing direction.</returns>
-		[BurstCompile]
+		//[BurstCompile]
 		public float angle () {
 			//Calculate the initial angle.
 			float returner = math.acos(math.dot(new float2(0,-1), normalized())) / (2*(float)math.PI);
@@ -43,7 +43,7 @@ namespace Clouds.Facing2D
 		public float signedAngle () {
 			return Vector2.SignedAngle(Vector2.up, math.normalize(Value));
 		}
-		[BurstCompile]
+		//[BurstCompile]
 		public float2 normalized () {
 			return math.normalize(Value);
 		}
