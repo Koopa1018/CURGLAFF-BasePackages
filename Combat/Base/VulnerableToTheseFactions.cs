23d4847db64a8e127cf48e9ecb87cc34a334ba6d
Koopa1018 <caleblovestoytrains@gmail.com>
2022-12-15 06:08:21 -0700

    Use consistent language throughout
    Hit reactors "require vulnerability map"--so you need to be able to search _vulnerability_!

--- Combat/Base/VulnerableToTheseFactions.cs
+++ Combat/Base/VulnerableToTheseFactions.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
 namespace Clouds.Combat {
-	[AddComponentMenu("Combat/Vulnerability Maps/Take Hits From These Factions:")]
-	public class HitByTheseFactions : MonoBehaviour, IVulnerabilityMap {
+	[AddComponentMenu("Combat/Vulnerability Maps/Vulnerable To These Factions:")]
+	public class VulnerableToTheseFactions : MonoBehaviour, IVulnerabilityMap {
 		[SerializeField] int[] factions = new int[1];
 
 		public float CheckAttack (IAttack attack) {
